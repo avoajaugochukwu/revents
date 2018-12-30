e6d4864d85cc1b7c16909e2fcacd3b037299b310
Avoaja Ugochukwu <karlthegoo@hotmail.com>
2018-12-30 11:36:10 +0100

    Added other needed pages

---
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingsDashboard = () => {
+  return (
+    <div>
+      <h1>Settings SettingsDashboard</h1>
+    </div>
+  )
+}
+
+export default SettingsDashboard
